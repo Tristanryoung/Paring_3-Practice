06dccaa0e67eac54e1fcd321105992cefee85a66
Tristan Young <tyoung22@turing.edu>
2023-01-12 21:46:03 -0800

    Add formatting

--- friends.js
+++ friends.js
@@ -1,11 +1,11 @@
 var numbers= [3, 20, 42, 39, 70, 81, 95, 97];
     for (i=0; i<numbers.length; i++) {
-        if (numbers[i] >30)
-        console.log(numbers[i])
-        for (i=0; i<numbers.length; i++) {
-            if (numbers[i]<60)
-            console.log(numbers[i])
-        }
+    if (numbers[i] >30)
+    console.log(numbers[i])
+    for (i=0; i<numbers.length; i++) {
+    if (numbers[i]<60)
+    console.log(numbers[i])
+    }
 }
    
 
